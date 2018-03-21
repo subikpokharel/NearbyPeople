24ff5b17e7dee743a41356ba545abfbeef46ee28
Subik Pokharel <subikpokharel93@gmail.com>
2018-03-21 13:02:15 -0500

    Working on Confirmation page

--- app/src/main/java/com/csci515/subik/peoplenearby/SendRequestActivity.java
+++ app/src/main/java/com/csci515/subik/peoplenearby/SendRequestActivity.java
@@ -1,15 +1,22 @@
 package com.csci515.subik.peoplenearby;
 
-import android.content.Intent;
+
+import android.app.TimePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
 
 public class SendRequestActivity extends AppCompatActivity {
 
     String[] array_details;
     TextView name, age, gender, resturant, address, time;
+    Button btn_confirm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +38,7 @@ public class SendRequestActivity extends AppCompatActivity {
         resturant = findViewById(R.id.resturant_name);
         address = findViewById(R.id.resturant_address);
         time = findViewById(R.id.text_time);
+        btn_confirm = findViewById(R.id.btnConfirm);
 
 
         name.setText(array_details[1]);
@@ -38,5 +46,22 @@ public class SendRequestActivity extends AppCompatActivity {
         gender.setText(array_details[3]);
         resturant.setText(array_details[6]);
         address.setText(array_details[7]);
+    }
+
+    public void setTime(View view) {
+
+        Calendar mcurrentTime = Calendar.getInstance();
+        int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY) - 5;
+        int minute = mcurrentTime.get(Calendar.MINUTE);
+        TimePickerDialog mTimePicker;
+        mTimePicker = new TimePickerDialog(SendRequestActivity.this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                time.setText( "Time of meeting: "+String.format("%02d : %02d",selectedHour, selectedMinute) );
+            }
+        }, hour, minute, true);//Yes 24 hour time
+        mTimePicker.setTitle("Select Time");
+        mTimePicker.show();
+        btn_confirm.setEnabled(true);
     }
 }
