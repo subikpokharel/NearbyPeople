04d8377b6c4576018e2d132635781a01fad9b285
Subik Pokharel <subikpokharel93@gmail.com>
2018-03-23 13:13:45 -0500

    Working on Appointment Adapter

--- app/src/main/java/com/csci515/subik/peoplenearby/AppointmentActivity.java
+++ app/src/main/java/com/csci515/subik/peoplenearby/AppointmentActivity.java
@@ -2,15 +2,21 @@ package com.csci515.subik.peoplenearby;
 
 import android.graphics.Typeface;
 import android.os.AsyncTask;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.csci515.subik.peoplenearby.myApplication.MyApplication;
 import com.csci515.subik.peoplenearby.parsing.Appointment;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -22,10 +28,11 @@ import java.util.ArrayList;
 
 public class AppointmentActivity extends AppCompatActivity {
 
-    TextView request_sent, request_received;
+    TextView request_sent, request_received, no_data;
     ListView listView;
     MyApplication myApplication;
-    ArrayList<Appointment> appointments;
+    View enable_view;
+    static ArrayList<Appointment> appointments = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +44,7 @@ public class AppointmentActivity extends AppCompatActivity {
 
         request_sent = findViewById(R.id.tvSent);
         request_received = findViewById(R.id.tvReceived);
+        no_data = findViewById(R.id.nodata);
         listView = findViewById(R.id.lv_appointments);
         myApplication = (MyApplication) getApplication();
         appointments = new ArrayList<Appointment>();
@@ -49,6 +57,7 @@ public class AppointmentActivity extends AppCompatActivity {
     View.OnClickListener listner = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
+            enable_view = v;
             switch (v.getId()){
 
                 case R.id.tvSent:
@@ -57,6 +66,7 @@ public class AppointmentActivity extends AppCompatActivity {
                     request_received.setTextColor(getResources().getColor(R.color.colorIcons));
                     request_sent.setTypeface(Typeface.DEFAULT_BOLD);
                     request_received.setTypeface(Typeface.DEFAULT);
+                    no_data.setVisibility(v.GONE);
                     getData("sent");
                     break;
 
@@ -67,6 +77,7 @@ public class AppointmentActivity extends AppCompatActivity {
                     request_sent.setTextColor(getResources().getColor(R.color.colorIcons));
                     request_received.setTypeface(Typeface.DEFAULT_BOLD);
                     request_sent.setTypeface(Typeface.DEFAULT);
+                    no_data.setVisibility(v.GONE);
                     getData("received");
                     break;
             }
@@ -75,18 +86,36 @@ public class AppointmentActivity extends AppCompatActivity {
 
     private void getData(String key) {
 
+        //String id = "6";
         String id = myApplication.getSavedValue("Id");
-        GetAppointment appointment = new GetAppointment();
+        final GetAppointment appointment = new GetAppointment();
         appointment.execute(key, id);
+
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Do something after 5s = 5000ms
+                if (appointments.size() == 0){
+                    no_data.setVisibility(enable_view.VISIBLE);
+                    //Toast.makeText(AppointmentActivity.this, "Empty result", Toast.LENGTH_LONG).show();
+                }else{
+                    //Toast.makeText(AppointmentActivity.this, "Not Empty result", Toast.LENGTH_LONG).show();
+                    no_data.setVisibility(enable_view.GONE);
+                }
+            }
+        }, 1500);
+
     }
 
 
 
-    private static class GetAppointment extends AsyncTask<String, Void, ArrayList<Appointment>>{
+    private static class GetAppointment extends AsyncTask<String, Void, String>{
 
         private String status;
         @Override
-        protected ArrayList<Appointment> doInBackground(String... strings) {
+        protected String doInBackground(String... strings) {
 
             status = strings[0];
             try {
@@ -98,7 +127,7 @@ public class AppointmentActivity extends AppCompatActivity {
                 conn.setDoOutput( true );
 
                 String data = URLEncoder.encode( "key", "UTF-8" ) + "=";
-                data += URLEncoder.encode( strings[0],   "UTF-8" ) + "&";
+                data += URLEncoder.encode( status,   "UTF-8" ) + "&";
                 data += URLEncoder.encode( "Id", "UTF-8" ) + "=";
                 data += URLEncoder.encode( strings[1],   "UTF-8" );
 
@@ -116,13 +145,43 @@ public class AppointmentActivity extends AppCompatActivity {
                     break;
                 }
                 Log.d("Output: ", sb.toString());
+                return sb.toString();
 
             }catch (Exception e){
                 Log.d("Exception connecting: " , e.getMessage());
             }
-
-
             return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+            try {
+                if (result != null){
+                    super.onPostExecute(result);
+                    JSONObject jsonRootObj = new JSONObject(result);
+                    JSONArray data = jsonRootObj.getJSONArray("details");
+                    appointments = new ArrayList<>(data.length());
+
+                    for (int i = 0; i < data.length(); i++) {
+                        JSONObject jsonObject = data.getJSONObject(i);
+
+                        String from_name = jsonObject.optString("From");
+                        String to_name = jsonObject.optString("To");
+                        String time = jsonObject.optString("Time");
+                        String latitude = jsonObject.optString("Latitude");
+                        String longitude = jsonObject.optString("Longitude");
+                        String resturant_name = jsonObject.optString("Res_Name");
+                        String resturant_address = jsonObject.optString("Address");
+                        int status = Integer.parseInt(jsonObject.optString("Status"));
+
+                        appointments.add(new Appointment(from_name, to_name, time, latitude, longitude, resturant_name, resturant_address, status));
+                    }
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
---
+++ app/src/main/java/com/csci515/subik/peoplenearby/adapter/AppointmentAdapter.java
@@ -0,0 +1,8 @@
+package com.csci515.subik.peoplenearby.adapter;
+
+/**
+ * Created by subik on 3/23/18.
+ */
+
+public class AppointmentAdapter {
+}
