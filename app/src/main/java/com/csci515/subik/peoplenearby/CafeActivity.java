30fa808abee0da30a2dfe82f8b57d99adad0a5e1
Subik Pokharel <subikpokharel93@gmail.com>
2018-03-28 12:02:17 -0500

    Nearest cafe located

--- app/src/main/java/com/csci515/subik/peoplenearby/CafeActivity.java
+++ app/src/main/java/com/csci515/subik/peoplenearby/CafeActivity.java
@@ -2,6 +2,7 @@ package com.csci515.subik.peoplenearby;
 
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.location.Location;
@@ -29,20 +30,27 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class CafeActivity extends AppCompatActivity {
 
     static String[] destination = null;
+    static Context mContext;
+    static String friend_id, dest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cafe);
-
+        mContext = CafeActivity.this;
         Intent intent = getIntent();
-        String friend_id = intent.getStringExtra("friendId");
-        String dest = intent.getStringExtra("destination");
+        friend_id = intent.getStringExtra("friendId");
+        dest = intent.getStringExtra("destination");
         destination = dest.split("/");
         /*double lat = Double.parseDouble(destination[0]);
         double lng = Double.parseDouble(destination[1]);*/
@@ -61,7 +69,7 @@ public class CafeActivity extends AppCompatActivity {
         String sensor = "sensor=true";
         // Building the parameters to the web service
         //String parameters = "location=" + latitude + "," + longitude + "&radius=500&type=food&name=cruise&key=" + API_Key;
-        String parameters = "location=" + lat + "," + lng + "&radius=500&type=cafe"+"&"+sensor+"&key=" + Web_Key;
+        String parameters = "location=" + lat + "," + lng + "&radius=00&type=cafe"+"&"+sensor+"&key=" + Web_Key;
         // Output format
         String output = "json";
         // Building the URL for the web serviceString
@@ -154,14 +162,15 @@ public class CafeActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(List<HashMap<String, String>> cafeNearby) {
             super.onPostExecute(cafeNearby);
-            ArrayList<String> distance = new ArrayList<>();
+            //ArrayList<String> distance = new ArrayList<>();
+            HashMap<String, Double> distance = new HashMap<>();
             for (int i = 0; i < cafeNearby.size(); i++) {
 
                 int Radius = 6371;// radius of earth in Km
                 HashMap<String, String> googlePlace = cafeNearby.get(i);
                 double lat = Double.parseDouble(googlePlace.get("lat"));
                 double lng = Double.parseDouble(googlePlace.get("lng"));
-
+                LatLng position = new LatLng(lat, lng);
                 double dLat = Math.toRadians(Double.parseDouble(destination[0]) - lat);
                 double dLon = Math.toRadians(Double.parseDouble(destination[1]) - lng);
                 double a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
@@ -170,14 +179,57 @@ public class CafeActivity extends AppCompatActivity {
                         * Math.sin(dLon / 2);
                 double c = 2 * Math.asin(Math.sqrt(a));
                 double valueResult = Radius * c;
-                double km = valueResult / 1;
-                DecimalFormat newFormat = new DecimalFormat("####");
-                int kmInDec = Integer.valueOf(newFormat.format(km));
-                double meter = valueResult % 1000;
-                int meterInDec = Integer.valueOf(newFormat.format(meter));
-                Log.i("Radius Value", "" + valueResult + "   KM  " + kmInDec
-                        + " Meter   " + meterInDec);
+                distance.put(String.valueOf(position),valueResult);
+                Log.i("Radius Value", "" + valueResult + "   KM  ");
+            }
+            ValueComparator bvc = new ValueComparator(distance);
+            TreeMap<String, Double> sorted_map = new TreeMap<String, Double>(bvc);
+            sorted_map.putAll(distance);
+            Log.i("First Value", "" + Arrays.asList(distance) );
+            Log.i("Sorted Value", "" + Arrays.asList(sorted_map) );
+
+            boolean value = sorted_map.isEmpty();
+            Log.i("Empty?? ", String.valueOf(value));
+            String key =  null;
+            if (!value)
+                key = sorted_map.keySet().toArray()[0].toString();
+            //Log.i("First element", key);
+
+            Intent intent = new Intent(mContext, TrackActivity.class);
+            intent.putExtra("destination",dest);
+            intent.putExtra("friendId", friend_id);
+            intent.putExtra("cafe", key);
+            mContext.startActivity(intent);
+
+            //sorted_map.firstKey()
+            /*String key = sorted_map.firstKey();
+            Double value = sorted_map.get()
+            Log.i("First element",)*/
+
+            /*Log.i("First Value", "" + Arrays.asList(distance) );
+            Log.i("Second Value", "" + Collections.singletonList(distance) );*/
+
+        }
+
+        class ValueComparator implements Comparator<String> {
+            Map<String, Double> base;
+
+            public ValueComparator(Map<String, Double> base) {
+                this.base = base;
+            }
+
+            // Note: this comparator imposes orderings that are inconsistent with
+            // equals.
+            public int compare(String a, String b) {
+                if (base.get(a) <= base.get(b)) {
+                    return -1;
+                } else {
+                    return 1;
+                } // returning 0 would merge keys
             }
         }
+
+
     }    // End of ParserTask
 }
+//[{lat/lng: (47.9120142,-97.0907593)=0.33285008964224927, lat/lng: (47.913926,-97.087469)=0.4318569223570965}]
